ae0537744abb0bcd9a37b8bf7aac2b7010e2259c
gentsapko <gentsapko@gmail.com>
2016-12-25 10:07:12 +0200

    Add_Domen_Models:_Purchase,Book.

---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Author { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ BookStore/BookStore/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+        // Shopping Id
+        public int PurchaseId { get; set; }
+        // The name and surname of the customer
+        public string Person { get; set; }
+        // Address of the buyer
+        public string Adress { get; set; }
+        // ID book
+        public int BookId { get; set; }
+        // The date of purchase
+        public DateTime Date { get; set; }
+    }
+}
