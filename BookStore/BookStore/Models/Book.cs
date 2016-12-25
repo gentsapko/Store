ce7393e21500d1a11694a4af751c6d3e4f17b751
gentsapko <gentsapko@gmail.com>
2016-12-25 19:49:18 +0200

    Add_Models_Class_BookDbInitializer.cs

--- BookStore/BookStore/Models/Book.cs
+++ BookStore/BookStore/Models/Book.cs
@@ -8,8 +8,8 @@ namespace BookStore.Models
     public class Book
     {
         public int Id { get; set; }
-        public int Name { get; set; }
-        public int Author { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
         public int Price { get; set; }
     }
 }
